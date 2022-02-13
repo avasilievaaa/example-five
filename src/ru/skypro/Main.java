1881b151716815315db925eca786f964368a0bbb
exampleOne <a.vasilievaaa@yandex.ru>
2022-02-13 18:38:24 +0300

    Домашнее задание №5 (Работа над ошибками)

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,41 +3,94 @@ package ru.skypro;
 import java.util.Arrays;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         // Задание №1
         //1.Целочисленный массив, заполненный тремя цифрами – 1, 2 и 3
-        int [] numbers = new int [12];
+        int [] numbers = new int [3];
         numbers [0] = 1;
         numbers [1] = 2;
         numbers [2] = 3;
 
         //2. Массив, в котором можно хранить три дробных числа – 1.57, 7.654, 9.986
-        double [] numbersTwo = {1.57,7.654,9.986,0,0,0,0,0,0,0,0,0};
+        double [] numbersTwo = {1.57,7.654,9.986};
 
         //3.Произвольный массив
-        int [] numbersThree = {90,80,110,0,0,0,0,0,0,0,0,0};
+        int [] numbersThree = {90,80,110};
 
         //Задание №2.
-        System.out.println(numbers[0] + "," + numbers[1] + "," + numbers[2]);
-        System.out.println(numbersTwo[0] + "," + numbersTwo[1] + "," + numbersTwo[2]);
-        System.out.println(numbersThree[0] + "," + numbersThree[1] + "," + numbersThree[2]);
+        // 1.
+        for (int i = 0; i < numbers.length; i++) {
+            if (i > 0) {
+                System.out.print(",");
+            }
+            System.out.print(numbers[i]);
+        }
+        System.out.println();
+
+        // 2.
+        for (int i = 0; i < numbersTwo.length; i++) {
+            if (i > 0) {
+                System.out.print(",");
+            }
+            System.out.print(numbersTwo[i]);
+        }
+        System.out.println();
+
+        // 3.
+        for (int i = 0; i < numbersThree.length; i++) {
+            if (i > 0) {
+                System.out.print(",");
+            }
+            System.out.print(numbersThree[i]);
+        }
+        System.out.println();
+
 
         //Задание №3
-        System.out.println(numbers[2] + "," + numbers[1] + "," + numbers[0]);
-        System.out.println(numbersTwo[2] + "," + numbersTwo[1] + "," + numbersTwo[0]);
-        System.out.println(numbersThree[2] + "," + numbersThree[1] + "," + numbersThree[1]);
+        // 1.
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i]);
+            if (i > 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+
+        // 2.
+        for (int i = numbersTwo.length - 1; i >= 0; i--) {
+            System.out.print(numbersTwo[i]);
+            if (i > 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+
+        //3.
+        for (int i = numbersThree.length - 1; i >= 0; i--) {
+            System.out.print(numbersThree[i]);
+            if (i > 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
 
         //Задание №4
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers [i] % 2 != 0) {
+            if (numbers [i] % 2 != 0)
                 numbers [i] = numbers [i] + 1;
-                System.out.println(numbers[0] + "," + numbers[1] + "," + numbers[2]);
+            if (i > 0) {
+                System.out.print(",");
             }
-            }
+            System.out.print(numbers[i]);
         }
-        }
+    }
+}
+
+
+
+
+
 
 
 
